697aba726326233bc046ff6bf56d130acd4f29b7
Joseph Adedeji Adewunmi <adedejiosvaldo>
2024-10-05 17:00:02 +0100

    Feature - Create Comment

---
+++ client/src/components/CommentCreate.tsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const CommentCreate = ({ postId }: { postId: string }) => {
+  const [Comment, setComment] = useState("");
+
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+
+    await axios.post(`http://localhost:5001/posts/${postId}/comments`, {
+      Comment,
+    });
+
+    setComment("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="">New Comment</label>
+          <input
+            type="text"
+            value={Comment}
+            onChange={(e) => setComment(e.target.value)}
+            className="form-control"
+            name=""
+            id=""
+          />
+        </div>
+        <br />
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default CommentCreate;
