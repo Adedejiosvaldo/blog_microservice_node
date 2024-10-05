d91777991b14ccc94a6e28d0c005579a04a28aad
Joseph Adedeji Adewunmi <adedejiosvaldo>
2024-10-05 15:13:05 +0100

    Feature - Create Post

--- client/src/components/PostCreate.tsx
+++ client/src/components/PostCreate.tsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
+import axios from "axios";
 
 const PostCreate = () => {
+  const [title, setTitle] = useState<string>("");
+
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+    await axios.post("http://localhost:4000/posts", {
+      title,
+    });
+    setTitle("");
+  };
+
   return (
     <div>
-      <form action="">
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           {/*  */}
           <label htmlFor="">Title</label>
-          <input type="text" className="form-control" />
+          <input
+            type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
-
-        <button className="btn btn-primary">Submit</button>
+        <br />
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </form>
     </div>
   );
