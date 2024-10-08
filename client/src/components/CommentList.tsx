f2021c58935a2bc035e2bab830f18a9496891c96
Joseph Adedeji Adewunmi <adedejiosvaldo@gmail.com>
2024-10-08 16:09:14 +0100

    Feature -> Updating the user interface

--- client/src/components/CommentList.tsx
+++ client/src/components/CommentList.tsx
@@ -1,6 +1,16 @@
 const CommentList = ({ comments }: { comments: [string] }) => {
-  const renderedPosts = comments.map((post: any) => {
-    return <li key={post.id}>{post.content}</li>;
+  const renderedPosts = comments.map((comment: any) => {
+    let content;
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedPosts}</ul>;
