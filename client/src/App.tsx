ed13da058e7df8123ad538d72a8ec8cd54e696c1
Joseph Adedeji Adewunmi <adedejiosvaldo@gmail.com>
2024-10-31 16:58:37 +0100

    Fix -> Fixed the dockerfile for the client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 // import "./App.css";
 import PostCreate from "./components/PostCreate";
 import PostList from "./components/PostList";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="container">
       <h1>Create Post</h1>
--- client/src/components/CommentCreate.tsx
+++ client/src/components/CommentCreate.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import  { useState } from "react";
 
 const CommentCreate = ({ postId }: { postId: string }) => {
   const [Comment, setComment] = useState("");
